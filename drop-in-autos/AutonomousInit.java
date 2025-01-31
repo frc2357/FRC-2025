5e44f2476d8a3bc0247266d57ce423de70bdd3f9
Kenneth430 <treygethers@gmail.com>
2025-01-30 19:38:10 -0600

    Made some Prettier fixes

--- drop-in-autos/AutonomousInit.java
+++ drop-in-autos/AutonomousInit.java
@@ -3,22 +3,28 @@ import edu.wpi.first.wpilibj.Timer;
 public Timer timer = new Timer();
 
 public void autonomousInit() {
-    var swerve = {};
-    timer.start();
-    swerve.setControl(
-        new SwerveRequest.FieldCentric()
-      .withDriveRequestType(DriveRequestType.OpenLoopVoltage).withVelocityX(-1).withVelocityY(0).withRotationalRate(0);
-    );
+  var swerve = {};
+  timer.start();
+  swerve.setControl(
+    new SwerveRequest.FieldCentric()
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+      .withVelocityX(-1)
+      .withVelocityY(0)
+      .withRotationalRate(0)
+  );
 }
 
 public void autonomousPeriodic() {
-    if (timer.hasElapsed(3)) {
-        var swerve = {};
-        swerve.setControl(
-            new SwerveRequest.FieldCentric()
-        .withDriveRequestType(DriveRequestType.OpenLoopVoltage).withVelocityX(-1).withVelocityY(0).withRotationalRate(0);
-        );
-        timer.reset();
-        timer.stop();
-    }
+  if (timer.hasElapsed(3)) {
+    var swerve = {};
+    swerve.setControl(
+      new SwerveRequest.FieldCentric()
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+        .withVelocityX(-1)
+        .withVelocityY(0)
+        .withRotationalRate(0)
+    );
+    timer.reset();
+    timer.stop();
+  }
 }
