306da28a228b2fb97f2b7cca5fab6b9cda6f3725
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-02-08 11:04:58 -0600

    work in progress

--- robotbase/src/main/java/frc/robot/commands/scoring/CoralPreposeScoreSelector.java
+++ robotbase/src/main/java/frc/robot/commands/scoring/CoralPreposeScoreSelector.java
@@ -1,12 +1,43 @@
 package frc.robot.commands.scoring;
 
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.Constants;
-import frc.robot.commands.elevator.ElevatorSetDistance;
-import frc.robot.commands.laterator.LateratorSetDistance;
+import java.util.Map;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
 import frc.robot.controls.controllers.ButtonboardController.ScoringLevel;
 
+public class CoralPreposeScore {
+
+
+ private Scoring PreposeSelector select(){
+  return ScoringLevel.L1;
+ }
+
+  private final Command m_selectCommand = 
+    new SelectCommand<>(
+      Map.ofEntries(
+        Map.entry(ScoringLevel.L1, new CoralPreposeL1()),
+        Map.entry(ScoringLevel.L2, new CoralPreposeL2()),
+        Map.entry(ScoringLevel.L3, new CoralPreposeL3()),
+        Map.entry(ScoringLevel.L4, new CoralPreposeL4())),
+      this::select);
+    
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
 public class CoralPreposeScore extends ParallelCommandGroup {
 
   private Distance m_lateratorDistance;
@@ -37,3 +68,4 @@ public class CoralPreposeScore extends ParallelCommandGroup {
     );
   }
 }
+*/
