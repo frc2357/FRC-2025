0b3d54a5e8c51047a0963d584f9dae2fe41a4d4b
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-02-08 11:38:04 -0600

    Chris/redo preposes (#46)
    * work in progress
    
    * figured out the SelectCommand and have implemented it properly (hopefully)

---
+++ robotbase/src/main/java/frc/robot/commands/scoring/CoralPreposeL1.java
@@ -0,0 +1,16 @@
+package frc.robot.commands.scoring;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.elevator.ElevatorSetDistance;
+import frc.robot.commands.laterator.LateratorSetDistance;
+
+public class CoralPreposeL1 extends ParallelCommandGroup {
+
+  public CoralPreposeL1() {
+    super(
+      new LateratorSetDistance(Constants.LATERATOR.SETPOINTS.L1_PREPOSE),
+      new ElevatorSetDistance(Constants.ELEVATOR.SETPOINTS.L1_PREPOSE)
+    );
+  }
+}
