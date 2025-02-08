8c2029e6f91fa96285596c881b820a8665f7f702
Tyson453 <tyson112305@gmail.com>
2025-02-08 17:57:08 -0600

    CPP makes my head hurt

--- buttonboard/buttonboard.ino
+++ buttonboard/buttonboard.ino
@@ -1,4 +1,5 @@
 #include "LevelSelection.h"
+#include "BranchSelection.h"
 
 #define LEFT_LEVEL_KEYPAD_ADDRESS 0x30
 #define RIGHT_LEVEL_KEYPAD_ADDRESS 0x31
@@ -7,14 +8,16 @@
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   XInput.begin();
 
   LevelSelection::init(LEFT_LEVEL_KEYPAD_ADDRESS, RIGHT_LEVEL_KEYPAD_ADDRESS, LEVEL_KEYPAD_INTERRUPT_PIN);
+  BranchSelection::init();
 }
 
 void loop()
 {
-
+  // LevelSelection and BranchSelection updates are handled with interrupts so nothing needs to happen here
 }
