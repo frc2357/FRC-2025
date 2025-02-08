3f20d47244c9a8452d52e65499c2dcb64beda965
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2025-02-08 11:40:43 -0600

    fixed build issues

--- robotbase/src/main/java/frc/robot/Robot.java
+++ robotbase/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -49,6 +50,7 @@ public class Robot extends TimedRobot {
   public static AlgaePivot algaePivot;
   public static DriverControls driverControls;
   public static ButtonboardController buttonboard;
+  public static ElasticFieldManager elasticFieldManager;
 
   public static Alliance alliance = null;
 
