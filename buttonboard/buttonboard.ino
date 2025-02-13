cbf4d7d6a3e85cb8a18e07e05b4f75e4a16e2fa0
Tyson453 <tyson112305@gmail.com>
2025-02-13 15:07:20 -0600

    Remove branch selection stuff so it can be in a different branch

--- buttonboard/buttonboard.ino
+++ buttonboard/buttonboard.ino
@@ -1,5 +1,4 @@
 #include "LevelSelection.h"
-#include "BranchSelection.h"
 
 #define LEFT_LEVEL_KEYPAD_ADDRESS 0x30
 #define RIGHT_LEVEL_KEYPAD_ADDRESS 0x31
@@ -14,10 +13,9 @@ void setup()
   XInput.begin();
 
   LevelSelection::init(LEFT_LEVEL_KEYPAD_ADDRESS, RIGHT_LEVEL_KEYPAD_ADDRESS, LEVEL_KEYPAD_INTERRUPT_PIN);
-  BranchSelection::init();
 }
 
 void loop()
 {
-  // LevelSelection and BranchSelection updates are handled with interrupts so nothing needs to happen here
+  // LevelSelection updates are handled with interrupts so nothing needs to happen here
 }
