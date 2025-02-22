68b335c54f2aa863abd3d3dd8643ded762b3c105
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-02-22 10:48:48 -0600

    implemented algae knocker can id

--- robotbase/src/main/java/frc/robot/subsystems/AlgaeKnocker.java
+++ robotbase/src/main/java/frc/robot/subsystems/AlgaeKnocker.java
@@ -6,13 +6,14 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ALGAE_KNOCKER;
+import frc.robot.Constants.CAN_ID;
 
 public class AlgaeKnocker extends SubsystemBase {
 
   private SparkMax m_motor;
 
   public AlgaeKnocker() {
-    m_motor = new SparkMax(0, MotorType.kBrushless);
+    m_motor = new SparkMax(CAN_ID.ALGAE_KNOCKER_MOTOR, MotorType.kBrushless);
 
     m_motor.configure(
       ALGAE_KNOCKER.MOTOR_CONFIG,
