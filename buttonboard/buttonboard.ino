4bd9bd93e9f6119ece6549c81bf6138eaa6eae95
Tyson453 <tyson112305@gmail.com>
2025-03-15 17:49:58 -0500

    buttonboard.ino changes

--- buttonboard/buttonboard.ino
+++ buttonboard/buttonboard.ino
@@ -9,10 +9,10 @@
 #define PANIC_CONTROLS_MCP_I2C_ADDRESS 0x20
 #define PANIC_CONTROLS_MCP_INT_PIN 1
 
-#define LEDS_MCP_I2C_ADDRESS 0x20
+#define LEDS_MCP_I2C_ADDRESS 0x21
 
-// LevelSelection level(LEFT_LEVEL_KEYPAD_ADDRESS, RIGHT_LEVEL_KEYPAD_ADDRESS);
-// BranchSelection branch;
+LevelSelection level(LEFT_LEVEL_KEYPAD_ADDRESS, RIGHT_LEVEL_KEYPAD_ADDRESS);
+BranchSelection branch;
 // PanicControls panic(PANIC_CONTROLS_MCP_I2C_ADDRESS, PANIC_CONTROLS_MCP_INT_PIN);
 Leds leds(LEDS_MCP_I2C_ADDRESS);
 
@@ -24,29 +24,28 @@ void setup()
 
   XInput.begin();
 
-  // level.init();
-  // branch.init();
+  level.init();
+  branch.init();
   // panic.init();
   leds.init();
 }
 
 void loop()
 {
-  // level.update();
-  // branch.update();
+  level.update();
+  branch.update();
   // panic.update();
-  delay(100);
-  // leds.update(level.getSelection(), branch.getSelection());
-  leds.update(LevelSelection::Level::L2, BranchSelection::Branch::A);
+  delay(10);
+  leds.update(level.getSelection(), branch.getSelection());
 }
 
 // Reef branch selection debouncer methods
 void onPinActivated(int pin)
 {
-  // branch.onPinActivated(pin);
+  branch.onPinActivated(pin);
 }
 
 void onPinDeactivated(int pin)
 {
-  // branch.onPinDeactivated(pin);
+  branch.onPinDeactivated(pin);
 }
