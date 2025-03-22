e64f67814d7b9b15ce0867cdc15eb1c22ab77f17
CStanley <150400256+PureBreadCat@users.noreply.github.com>
2025-03-22 10:42:34 -0500

    placed it all within an instantcommmand incase the CoralRetract is canceled before the rumble stops

--- robotbase/src/main/java/frc/robot/commands/intake/CoralRetract.java
+++ robotbase/src/main/java/frc/robot/commands/intake/CoralRetract.java
@@ -4,11 +4,12 @@ import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.rumble.RumbleDriverController;
 import frc.robot.commands.scoring.CoralHome;
 
-public class CoralRetract extends ParallelCommandGroup {
+public class CoralRetract extends SequentialCommandGroup {
 
   public CoralRetract() {
     this(true);
@@ -17,16 +18,13 @@ public class CoralRetract extends ParallelCommandGroup {
   public CoralRetract(boolean zero) {
     super(
       new CoralHome(() -> zero).alongWith(new CoralSettle()),
-      new ConditionalCommand(
-        new RumbleDriverController(),
-        new InstantCommand(),
-        () -> {
-          return (
-            Robot.coralRunner.isOuttakeBeamBroken() &&
-            !RobotState.isAutonomous()
-          );
+      new InstantCommand(() -> {
+        if (
+          Robot.coralRunner.isOuttakeBeamBroken() && !RobotState.isAutonomous()
+        ) {
+          new RumbleDriverController().schedule();
         }
-      )
+      })
     );
   }
 }
